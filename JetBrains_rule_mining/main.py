6adaaf65d60193f02c22ae66cf4fdc9d2d4346fd
KarcinMubacki <129968334+KarcinMubacki@users.noreply.github.com>
2025-04-04 13:33:53 +0200

    Update main.py

--- JetBrains_rule_mining/main.py
+++ JetBrains_rule_mining/main.py
@@ -21,6 +21,11 @@ def parse_args():
     return parser.parse_args()
 
 def main():
+    args = parse_args()
+    with open(args.rules) as f:
+        rules_raw = f.read()
+    df = pd.read_csv(args.data, sep="\t")
+    functionalities.parse_and_prune_ruleset(rules_raw, df, min_support=0.1, min_confidence=0.1)
     return
 
 if __name__ == '__main__':
